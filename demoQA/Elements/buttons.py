3b7686a6a1869e0044f9c496e62f99eb21137567
RB <renaldibong@gmail.com>
2024-11-04 17:06:13 +0700

    Add automation for Buttons section

---
+++ demoQA/Elements/buttons.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("https://demoqa.com/buttons")
+
+action = ActionChains(driver)
+
+doubleClick = driver.find_element(By.ID, 'doubleClickBtn')
+action.double_click(doubleClick).perform()
+time.sleep(3)
+
+# You can use this too
+# 
+# doubleClick = WebDriverWait(driver,10).until(
+#     EC.element_to_be_clickable((By.ID, 'doubleClickBtn'))
+# )
+# action.double_click(doubleClick).perform()
+
+rightClick = driver.find_element(By.ID, 'rightClickBtn')
+action.context_click(rightClick).perform()
+
+time.sleep(3)
+
+dynamicClick = driver.find_element(By.XPATH, "//button[text()='Click Me']")
+# action.click(dynamicClick).perform()
+dynamicClick.click()
+
+time.sleep(3)
